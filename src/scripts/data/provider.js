21e1dfe70a3ac68be3bcbd490e00ab72605dc3c1
Kristi Cornett <kristi@cornettclan.com>
2023-02-16 11:05:22 -0600

    inital database

--- src/scripts/data/provider.js
+++ src/scripts/data/provider.js
@@ -10,3 +10,5 @@ const applicationState = {
         displayMessages: false
     }
 }
+
+
--- src/scripts/main.js
+++ src/scripts/main.js
@@ -11,3 +11,4 @@ export const renderApp = () => {
         applicationElement.innerHTML = LoginForm()
     }
 }
+renderApp()
