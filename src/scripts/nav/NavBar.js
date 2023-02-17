3145098b3bae88a308b794adfca17727c2aa320f
Dylan Glynn <dylan.hawes.glynn+github@gmail.com>
2023-02-17 10:44:07 -0600

    NavBar functionality, updated GiffyGram module to show route HTML string, updated CustomEvent.

--- src/scripts/nav/NavBar.js
+++ src/scripts/nav/NavBar.js
@@ -1,5 +1,4 @@
 import { applicationElement } from "../main.js";
-import { setDisplayMessages } from "../data/provider.js";
 
 export const NavBar = () => {
     // Home Icon | Giffygram-h1 -- CreateDM UserDMReceived Logout
@@ -43,28 +42,6 @@ document.addEventListener("click", event => {
 
 document.addEventListener("click", event => {
     if (event.target.id === "directMessageIcon") {
-        setDisplayMessages()
         applicationElement.dispatchEvent(new CustomEvent("stateChanged"))
     }
 })
-
-// Copied from GiffyGram
-/*
-import { setDisplayMessages } from "./data/provider.js"
-import { NavBar } from "./nav/NavBar.js"
-import { postList } from "./feed/PostList.js"
-import { DirectMessage } from "./friends/DirectMessage.js"
-import { MessageForm } from "./MessageForm.js"
-import { Footer } from "./Footer.js"
-
-export const GiffyGram = () => {
-    return `
-        ${NavBar()}
-        `
-    /* Import modules for:
-    a. ${PostList()}
-    b. ${MessageForm()}
-    C. ${DirectMessage()}
-    d. ${Footer()}
-}
-*/
