026e4886e107be0fa48feca7118dbd6f26695c4c
Annaliese Mathews <amesibov043@gmail.com>
2023-02-20 13:42:39 -0600

    footer filters by date and favorites (in addition to user)

--- src/scripts/data/provider.js
+++ src/scripts/data/provider.js
@@ -6,7 +6,8 @@ const applicationState = {
     feed: {
         chosenUser: null,
         displayFavorites: false,
-        displayMessages: false
+        displayMessages: false, 
+        setDate: null
     },
     users: [],
     posts: [],
@@ -34,6 +35,21 @@ export const getChosenUser =() => {
 export const setUser = (id) => {
     applicationState.feed.chosenUser = id
     applicationElement.dispatchEvent(new CustomEvent('stateChanged'))
+}
+
+export const getChosenDate = () => {
+    return applicationState.feed.setDate
+}
+
+export const setDate = (id) => {
+    applicationState.feed.setDate = id
+    applicationElement.dispatchEvent(new CustomEvent('stateChanged'))
+}
+
+export const setOnlyFav = () => {
+    applicationState.feed.displayFavorites = !applicationState.feed.displayFavorites
+
+    console.log(applicationState.feed.displayFavorites)
 }
 
 export const getFeed = () => {
--- src/scripts/nav/Footer.js
+++ src/scripts/nav/Footer.js
@@ -1,20 +1,39 @@
-import { getUsers, getPosts, getFavorites, setUser,getChosenUser } from "../data/provider.js";
+import { getUsers, getPosts, getFavorites, setOnlyFav, setUser,getChosenUser, getFeed, setDate, getChosenDate } from "../data/provider.js";
 import { applicationElement } from "../main.js";
+
+let years = [2023, 2022, 2021, 2020]
 
 export const Footer = () => {
     const users = getUsers()
     const posts = getPosts()
     const chosenUserId = getChosenUser()
-    return `<div class="footer">
+    const chosenDate = getChosenDate()
+    const feed = getFeed()
+
+    let html = ""
+    html += `<div class="footer">
     <label class="year" for="year">Posts since </label>
-        <select class="year" id="year">
-            <option value="1">2023</option>
-            <option value="2">2022</option>
-            <option value="3">2021</option>
-            <option value="4">2020</option>
-        </select> 
-     ${posts.length}
-    <label class="users" for="users">Posts by user </label>
+        <select class="year" id="year"> <option value="0">Choose a year</option>
+        ${
+            years.map(
+                year => {
+                    let selected = (year === chosenDate) ? 'selected' : ''
+                    return  `<option value="${year}" ${selected}> ${year}</option>`
+                }
+            ).join("")
+        }
+        </select>`
+        if (chosenUserId){
+            let num = null
+            for(const post of posts){
+                if(post.userId === chosenUserId){
+                    num += 1
+                }
+            } html += `${num}  `
+        } else {
+            html += `${posts.length}  `
+        }
+    html +=`<label class="users" for="users">Posts by user </label>
         <select class="users" id="users"> <option value="0">Choose An Option</option>
             ${
                 users.map(
@@ -24,29 +43,22 @@ export const Footer = () => {
                         }
                 ).join("")
             }
-        </select>
-        <label class="favorite" for="favorite">Show only favorites </label>
-            <input type="radio" name="favorite" value="favorite" />
+        </select>`
+        if (feed.displayFavorites){
+            html +=`<label class="favorite" for="favorite">Show only favorites </label> <input type="checkbox" name="favorite" checked="checked" value="favorite" />
+            </div>
+            `
+        } else {
+            html +=`<label class="favorite" for="favorite">Show only favorites </label> <input type="checkbox" name="favorite" value="favorite" />
         </div>
         `
+        }
+            
+
+    return html
 }
 
-const chosenUserPosts = (post) => {
-    const users = getUsers()
-     let html = ""
-     html += `<div class="giffygram__feed"> <h3> ${post.name} </h3> <img class="post__image" src="${post.link}"> <p> ${post.message} </p>`
-         for (const user of users) {
-             if(user.id === parseInt(post.userId)){
-                 html += `<p> Posted by ${user.name} on ${post.datePosted} </p>`
-             
-         }
-     }
-     return html
-    }
-
 document.addEventListener("change", event => {
-    const miniMode = document.querySelector("#form")
-    const posts = getPosts()
     if (event.target.id === "users") {
         const userId = parseInt(event.target.value)
         setUser(userId)
@@ -56,16 +68,20 @@ document.addEventListener("change", event => {
     
 })
 
-/* document.addEventListener("change", event => {
-    if (event.target.id === "favorite") {
-        applicationState.feed.displayFavorites = true
+document.addEventListener("change", event => {
+    if (event.target.id === "year") {
+        const dateId = parseInt(event.target.value)
+        setDate(dateId)
+      
+        applicationElement.dispatchEvent(new CustomEvent("stateChanged"))
     }
-    if (applicationState.feed.disp) {
-        for (const post of posts) {
-            if(applicationState.feed.chosenUser === post.userId) {
-                
-            }
-        }
+    
+})
+
+document.addEventListener("change", event => {
+    if (event.target.name === "favorite") {
+        setOnlyFav()
+        applicationElement.dispatchEvent(new CustomEvent("stateChanged"))
     }
 })
- */
+ 
